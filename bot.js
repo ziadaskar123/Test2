fe9b05c7fdc5e07c985fd7ddc6ff9f6469f23382
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 12:09:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,18 +13,3 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'Hello') {
-    	message.reply('Hey');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);

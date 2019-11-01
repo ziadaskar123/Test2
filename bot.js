6c6acba149f019a3f3b4538ed93ce26d7a4eac02
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 02:07:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,16 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+} if (message.content.startsWith(config.prefix + "avatar")) {
+      if (!message.mentions.users.size) {
+        const avatarAuthor = new Discord.RichEmbed()
+      .setColor(0x333333)
+      .setAuthor(message.author.username)
+      .setImage(message.author.avatarURL)
+        message.channel.send(avatarAuthor);
+        let mention = message.mentions.members.first();
+        const avatarMention = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(mention.user.username)
+        .setImage(mention.user.avatarURL)
+        message.channel.send(avatarMention);

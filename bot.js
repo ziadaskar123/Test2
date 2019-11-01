81967689ed55b2bccb6d107d9ac274baf6a829ce
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 02:27:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,20 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'what is my avatar') {
+    message.reply(message.author.avatarURL);
+  }
+});
+client.login('NjM5NjAyNTc1ODE3MDQ4MDg2.Xbt5Mg.uVKgHyjunJ3lyPvLFc6IKiFloFI');

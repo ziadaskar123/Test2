da6b4519cead06b53e33eeee17a162806425e644
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 02:51:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,15 +2,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'Roy is Gay') {
-    	message.reply('i Agree');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
-npm install discord.js
+client.login('NjM5NjAyNTc1ODE3MDQ4MDg2.Xbt5Mg.uVKgHyjunJ3lyPvLFc6IKiFloFI');

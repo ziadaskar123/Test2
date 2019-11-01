58f68a05862954be68f5896b0b79fc00591f88fa
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 11:28:05 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,10 +1,33 @@
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	execute(message, args) {
-		message.channel.send('Pong.');
-	},
-};
+	/**
+ * Send a user a link to their avatar
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('NjM5NjAyNTc1ODE3MDQ4MDg2.Xbv3ZQ.j_pNqeGa4Uo0TLFdQBd4MpGZeUM');
 + const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');

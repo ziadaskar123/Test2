088e26d2de40f9a98215adaa0703c62de0329a85
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 11:37:14 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,1 +1,10 @@
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+client.on('message', message => {
+  if (message.content === 'what is my avatar') {
+    message.reply(message.author.avatarURL);
+  }
+});

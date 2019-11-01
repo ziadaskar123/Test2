928e04840b1e88ed8c6c35da06186b9aebd3eaaa
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 02:25:29 +0200

    Delete Commands.js

--- Commands.js
+++
@@ -1,8 +0,0 @@
-if (message.content.startsWith(config.prefix + 'avatar')) {
-    const user = message.mentions.users.first() || message.author;
-    const avatarEmbed = new Discord.RichEmbed()
-        .setColor(0x333333)
-        .setAuthor(user.username)
-        .setImage(user.avatarURL);
-    message.channel.send(avatarEmbed);
-}

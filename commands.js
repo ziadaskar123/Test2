ea750fe61268a418bca0752e18024372a0141ae4
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 11:41:03 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,10 +1,8 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-client.on('message', message => {
-  if (message.content === 'Avatar') {
-    message.reply(message.author.avatarURL);
-  }
-});
+if (message.content.startsWith(config.prefix + 'avatar')) {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.avatarURL);
+    message.channel.send(avatarEmbed);
+}

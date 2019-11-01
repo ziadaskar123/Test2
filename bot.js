cf8180361496af129cfb5c9541f1bdbdb263f696
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 15:00:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'stfu') {
+    	message.reply('no u');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});

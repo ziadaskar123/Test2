e8b90e72e7ed2a33e90149e3aac8736a165b984b
ziadaskar123 <57239318+ziadaskar123@users.noreply.github.com>
2019-11-01 12:05:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,9 +13,3 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log('I am ready!');
-    <client>.user.setActivity('<activity>', { type: 'WATCHING' });
